7e7f525a9e90c02fed059779dd304816fd0db924
China Brahma <brahmachina143@gmail.com>
2025-01-17 22:33:55 +0530

    Rambo Counter App

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCount] = useState(10)
+  // let counter = 10;
+
+  const increase=()=>{
+    if(counter<20){
+      counter = counter + 1;
+      setCount(counter)
+      console.log("Increased :"+counter)
+    }else{
+        alert('Limit exceeded, more than '+{counter});
+    }
+    
+  };
+  const decrease=()=>{
+    if(counter != 0){
+      counter = counter - 1;
+      setCount(counter)
+      console.log("Decreased :"+counter)
+    }else{
+      alert('Rambo can do better, atleast 1 job')
+    }
+  };
+
+  return (
+    <>
+      <div className='card'>
+     <h1>Rambo is on fire</h1>
+
+     <h3>Rambo has {counter} income stream</h3>
+     <button className='b1'
+      onClick={increase} 
+    >More</button>
+     <button
+      className='b1'
+      onClick={decrease}
+     >Less</button>
+
+
+     <p>Working on over {counter} of high paying jobs.</p>
+     </div>
+    </>
+  )
+}
+
+export default App
